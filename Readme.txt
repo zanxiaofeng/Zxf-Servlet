3ae060cdf797092bf46f6c34c77509547b42dfc6
zanxiaofeng <zanxiaofeng@163.com>
2021-07-16 14:12:35 +0800

    init commit

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+#Run
+./gradlew appRun
+
+#Test in browser
+http://localhost:8080/Zxf-Servlet/my-servlet
+
+http://localhost:8080/Zxf-Servlet/my-servlet?session=true
+
---
+++ src/main/java/zxf/practices/servlet/MyServlet.java
@@ -0,0 +1,25 @@
+package zxf.practices.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "myServlet", urlPatterns = "/my-servlet", loadOnStartup = 1)
+public class MyServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Boolean hasSession = request.getSession(false) != null;
+        Long time = hasSession ? (Long)request.getSession(false).getAttribute("time"):0L;
+        System.out.println("MyServlet       ::doGet::Start::Name=" + Thread.currentThread().getName() + "::ID=" + Thread.currentThread().getId() + " HasSession=" + hasSession + ", time=" + time);
+        if (!hasSession && "true".equals(request.getParameter("session"))){
+            request.getSession(true).setAttribute("time", System.currentTimeMillis());
+        }
+        try (OutputStream outputStream = response.getOutputStream()){
+           outputStream.write(("Session=" + hasSession + ", time="+ time).getBytes(StandardCharsets.UTF_8));
+        }
+    }
+}
